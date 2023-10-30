d3f884a279f123a4dede69e93f92b0dca748d33e
Tempest <l1641101764@gmail.com>
2023-10-30 22:25:50 +0800

    change style

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="container flex justify-around py-8 mx-auto bg-white border">
+    <nav class="w-screen flex justify-around py-8 mx-auto bg-white border-b">
       <div>
         <h3 class="text-2xl font-medium text-blue-500">LOGO</h3>
       </div>
@@ -7,7 +7,7 @@
         <RouterLink to="/">主页</RouterLink>
         <RouterLink to="/login">登录</RouterLink>
         <RouterLink to="/register">注册</RouterLink>
-        <button @click="Logout">退出</button>
+        <button @click="Logout" class="justify-end">退出</button>
       </div>
     </nav>
    </template>
