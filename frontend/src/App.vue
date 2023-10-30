cabab1f76bff6542e1089c5bc46b88b6a3d56815
Tempest <l1641101764@gmail.com>
2023-10-30 18:47:27 +0800

    simple login

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,12 @@
 <script setup lang="ts">
+  import {RouterView} from 'vue-router'
+  import Navbar from "./components/Navbar.vue";
 
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo"/>
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo"/>
-    </a>
-    <p class="text-3xl font-bold underline text-red-400">hello world</p>
-  </div>
+  <Navbar></Navbar>
+ <RouterView></RouterView>
   
 </template>
 
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav class="container flex justify-around py-8 mx-auto bg-white border">
+      <div>
+        <h3 class="text-2xl font-medium text-blue-500">LOGO</h3>
+      </div>
+      <div class="flex items-center space-x-8">
+        <RouterLink to="/">主页</RouterLink>
+        <RouterLink to="/login">登录</RouterLink>
+        <RouterLink to="/register">注册</RouterLink>
+        <button @click="Logout">退出</button>
+      </div>
+    </nav>
+   </template>
+   
+   <script lang="ts">
+   import { useRouter } from 'vue-router';
+   export default {
+    setup() {
+      const router = useRouter();
+      const Logout = () => {
+        localStorage.clear();
+        router.push({ path: '/login' });
+      };
+      return { Logout };
+    },
+   };
+   </script>
+   
