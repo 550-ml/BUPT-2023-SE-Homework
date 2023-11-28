70d194b2e0393b6c8db47e684f1292127f42103b
tadehaobaba <2461233967@qq.com>
2023-11-28 17:26:24 +0800

    feat: login,register,adm finish

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,6 @@
-<script setup lang="ts">
-  import {RouterView} from 'vue-router'
-  import Navbar from "./components/Navbar.vue";
-
-</script>
 
 <template>
-  <Navbar></Navbar>
   <RouterView></RouterView>
-  
 </template>
 
 <!-- <style scoped>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,9 +1,9 @@
 import {createRouter, createWebHistory} from 'vue-router'
 
-import HomeView from '../views/HomeView.vue'
-import Login from '../views/auth/Login.vue'
-import Register from '../views/auth/Register.vue'
-
+import HomeView from '../views/Homeview.vue'
+import Login from '../views/login/index.vue'
+import Register from '../views/login/register.vue'
+import Administrator from '../views/Administrator/Administrator.vue'
 const routes = [
    {
        path: '/',
@@ -19,7 +19,15 @@ const routes = [
        path: '/register',
        name: 'register',
        component: Register
-   }
+   },
+   {
+       path: '/administrator',
+       name: 'administrator',
+       component: Administrator
+   }, 
+   
+   
+
 ];
 
 const router = createRouter({
