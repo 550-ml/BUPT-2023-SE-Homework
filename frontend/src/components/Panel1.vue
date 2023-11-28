38467f8fb7b3b6080e73f265d3a69f845c58b33d
Tempest <l1641101764@gmail.com>
2023-11-28 22:17:07 +0800

    reformatted

---
+++ frontend/src/components/Panel1.vue
@@ -0,0 +1,8 @@
+<!-- Panel1.vue -->
+<template>
+    <div>
+      <h1 class="text-3xl font-semibold mb-4">Panel 1 Content</h1>
+      <p>Panel 1 的内容...</p>
+    </div>
+  </template>
+  
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,11 @@
-  // tailwind.config.js
-  module.exports = {
-    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-     darkMode: false, // or 'media' or 'class'
-     theme: {
-       extend: {},
-     },
-     variants: {
-       extend: {},
-     },
-     plugins: [],
-   }
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: "sans-serif"
+      }
+    }
+  },
+  plugins: []
+};
