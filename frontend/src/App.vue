a8419d25d74c8d2057547f37267030c247ca4f03
Tempest <l1641101764@gmail.com>
2023-11-28 20:38:27 +0800

    change

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
   import {RouterView} from 'vue-router'
-  import Navbar from "./components/Navbar.vue";
+  // import Navbar from "./components/Navbar.vue";
 
 </script>
 
 <template>
-  <Navbar></Navbar>
+  <!-- <Navbar></Navbar> -->
   <RouterView></RouterView>
-  
 </template>
 
 <!-- <style scoped>
