25b1f62247e53ae2dd2ae4c765dd0d750df6fb1c
Tempest <l1641101764@gmail.com>
2023-11-28 22:17:43 +0800

    reformatted

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,14 @@
-import {createApp} from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import router from "./router";
 // 导入element-plus整体
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-const app = createApp(App)
-app.use(ElementPlus)
-app.mount('#app')
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+const app = createApp(App);
+app.use(ElementPlus);
+// app.mount('#app')
 // 导入style 文件
-import './styles/index.scss'
+import "./styles/index.scss";
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount("#app");
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,38 +1,35 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
-import HomeView from '../views/Homeview.vue'
-import Login from '../views/login/index.vue'
-import Register from '../views/login/register.vue'
-import Administrator from '../views/Administrator/Administrator.vue'
+import HomeView from "../views/HomeView.vue";
+import Login from "../views/login/index.vue";
+import Register from "../views/login/register.vue";
+import Administrator from "../views/Administrator/Administrator.vue";
 const routes = [
-   {
-       path: '/',
-       name: 'home',
-       component: HomeView
-   },
-   {
-       path: '/login',
-       name: 'login',
-       component: Login
-   },
-   {
-       path: '/register',
-       name: 'register',
-       component: Register
-   },
-   {
-       path: '/administrator',
-       name: 'administrator',
-       component: Administrator
-   }, 
-   
-   
-
+    {
+        path: "/",
+        name: "home",
+        component: HomeView
+    },
+    {
+        path: "/login",
+        name: "login",
+        component: Login
+    },
+    {
+        path: "/register",
+        name: "register",
+        component: Register
+    },
+    {
+        path: "/administrator",
+        name: "administrator",
+        component: Administrator
+    }
 ];
 
 const router = createRouter({
-   history:createWebHistory(import.meta.env.BASE_URL),
-   routes,
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes
 });
 
-export default router
+export default router;
