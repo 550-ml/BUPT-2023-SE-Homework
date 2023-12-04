daa0955d8c61111001ddeae8237b227704e610e7
Tempest <l1641101764@gmail.com>
2023-12-04 21:52:37 +0800

    add tab

--- frontend/src/router/BillGenerate.vue
+++ frontend/src/router/BillGenerate.vue
@@ -1,40 +1,83 @@
 <template>
   <div class="w-full px-2 my-4">
-    <div class="flex items-center justify-end mb-4 mr-4">
-      <!-- 搜索框 -->
-      <input
-        type="text"
-        placeholder="请输入房间号"
-        class="p-2 border border-gray-300 rounded mr-2"
-        v-model="searchTerm"
-        @input="emitSearch"
-      />
-      <button class="bg-blue-500 text-white py-2 px-4 rounded" @click="executeSearch">搜索</button>
+    <div class="flex items-center justify-between mb-4 mr-4">
+      <div class="flex justify-start">
+        <button @click="changeTab('tab1')" class="py-2 px-4 rounded cursor-pointer">
+          <span class="">未入住房间</span>
+        </button>
+        <button @click="changeTab('tab2')" class="py-2 px-4 rounded cursor-pointer">
+          <span class="">已入住房间</span>
+        </button>
+      </div>
+
+      <div class="flex justify-end">
+        <input
+          type="text"
+          placeholder="请输入房间号"
+          class="p-2 border border-gray-300 rounded mr-2"
+          v-model="searchTerm"
+          @input="emitSearch"
+        />
+        <button class="bg-blue-500 text-white py-2 px-4 rounded" @click="executeSearch">搜索</button>
+      </div>
     </div>
-    <!-- <table class="table-auto w-full rounded-lg shadow-md">
-      <thead class="">
-        <tr>
-          <th>房间号</th>
-          <th>状态</th>
-          <th>入住时间</th>
-          <th>退房时间</th>
-          <th>总金额</th>
-        </tr>
-      </thead>
-      <tbody>
-        <RoomStates v-for="index in 4" :key="index" />
-      </tbody>
-    </table> -->
-    <div v-if="deviceData" class="flex justify-center">
-    <table class="border-collapse w-60 mt-8 shadow-md rounded-md overflow-hidden">
-      <tbody>
-        <tr v-for="(value, key) in deviceData" :key="key">
-          <th class="border p-2 bg-gray-100">{{ key }}</th>
-          <td class="border p-2">{{ value }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+
+    <div v-if="deviceData">
+      <div class="flex justify-center">
+        <table class="border-collapse w-60 mt-8 shadow-md rounded-md overflow-hidden">
+          <tbody>
+            <tr v-for="(value, key) in deviceData" :key="key">
+              <th class="border p-2 bg-gray-100">{{ key }}</th>
+              <td class="border p-2">{{ value }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div class="flex justify-center items-center py-4 space-x-4">
+        <button class="bg-blue-500 text-white py-2 px-4 rounded item-center" @click="openCheckOut">
+          生成账单并退房
+        </button>
+        <button class="bg-blue-500 text-white py-2 px-4 rounded item-center" @click="openDetailedOrder">
+          生成详单
+        </button>
+      </div>
+
+      <div class="flex justify-center items-center py-4">
+        <button class="bg-blue-500 text-white py-2 px-4 rounded item-center" @click="cancelSearch">返回列表</button>
+      </div>
+    </div>
+    <div v-else>
+      <div class="flex justify-center">
+        <div v-if="activeTab === 'tab1'" class="mt-4">
+          <table class="table-auto w-full rounded-lg shadow-md">
+            <thead class="">
+              <tr>
+                <th>房间号</th>
+              </tr>
+            </thead>
+            <tbody>
+              <RoomStates v-for="index in 4" :key="index" />
+            </tbody>
+          </table>
+        </div>
+        <div v-else-if="activeTab === 'tab2'" class="mt-4">
+          <table class="table-auto w-full rounded-lg shadow-md">
+            <thead class="">
+              <tr>
+                <th>房间号</th>
+                <th>入住时间</th>
+                <th>总金额</th>
+              </tr>
+            </thead>
+            <tbody>
+              <RoomStates v-for="index in 4" :key="index" />
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+
     <div v-if="errorMessage" class="modal">
       <div class="modal-content">
         <p>{{ errorMessage }}</p>
@@ -42,10 +85,25 @@
       </div>
     </div>
   </div>
+
+  <div v-if="isCheckOutOpen" class="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
+    <div class="bg-white p-6 rounded shadow-md">
+      <!-- 对话框内容 -->
+      <h2>xxx房间账单</h2>
+      <button @click="closeCheckOut" class="mt-4 bg-blue-500 text-white py-2 px-4 rounded">关闭对话框</button>
+    </div>
+  </div>
+  <div v-if="isDetailedOrderOpen" class="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
+    <div class="bg-white p-6 rounded shadow-md">
+      <!-- 对话框内容 -->
+      <h2>xxx房间详单</h2>
+      <button @click="closeDetailedOrder" class="mt-4 bg-blue-500 text-white py-2 px-4 rounded">关闭对话框</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import RoomStates from "../components/RoomStateHistory.vue";
+import RoomStates from "../components/RoomState.vue";
 import axios from "axios";
 
 export default {
@@ -55,7 +113,10 @@ export default {
   data() {
     return {
       searchTerm: "", // 用于存储搜索框的值
-      deviceData: null
+      deviceData: null,
+      isCheckOutOpen: false,
+      isDetailedOrderOpen: false,
+      activeTab: "tab1"
     };
   },
   methods: {
@@ -99,10 +160,44 @@ export default {
         last_update: "2023-09-18T11:45:14+08:00"
       };
     },
+    cancelSearch() {
+      this.deviceData = null;
+    },
+    openCheckOut() {
+      this.isCheckOutOpen = true;
+    },
+    closeCheckOut() {
+      this.isCheckOutOpen = false;
+    },
+    openDetailedOrder() {
+      this.isDetailedOrderOpen = true;
+    },
+    closeDetailedOrder() {
+      this.isDetailedOrderOpen = false;
+    },
     closeModal() {
       // 关闭对话框
       this.errorMessage = null;
+    },
+    changeTab(tab) {
+      this.activeTab = tab;
     }
   }
 };
 </script>
+
+<style scoped>
+button span::after {
+  content: "";
+  display: block;
+  width: 0;
+  height: 2px;
+  background-color: #3182ce;
+  transition: width 0.1s ease;
+}
+
+button:hover span::after,
+button:focus span::after {
+  width: 100%;
+}
+</style>
