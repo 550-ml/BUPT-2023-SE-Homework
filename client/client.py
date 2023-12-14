6780b28e72e7914afdd8716ac9c5a3eb7872d567
dzy <1353496364@qq.com>
2023-12-14 18:09:12 +0800

    test data change

--- client/client.py
+++ client/client.py
@@ -14,14 +14,14 @@ from Ui_new_client import Ui_Form
 
 class AirConditionerPanel(QWidget, Ui_Form):
 
-    def start_server(self):
-        try:
-            # 启动服务器的命令
-            server_command = "python server.py"
-            # 使用subprocess模块启动服务器
-            subprocess.Popen(server_command, shell=True)
-        except Exception as e:
-            print(f"Error starting server: {e}")
+    #def start_server(self):
+    #    try:
+    #        # 启动服务器的命令
+    #        server_command = "python server.py"
+    #        # 使用subprocess模块启动服务器
+    #        subprocess.Popen(server_command, shell=True)
+    #    except Exception as e:
+    #        print(f"Error starting server: {e}")
     def __init__(self):
         super().__init__()
         self.setupUi(self)
