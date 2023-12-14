314e2ad5fffd285874f5f3acf06a7294f0c23609
Tempest <l1641101764@gmail.com>
2023-12-14 19:19:39 +0800

    simplify

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,7 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
-// import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
-  <!-- <Navbar></Navbar> -->
   <RouterView></RouterView>
 </template>
-
-<!-- <style scoped>
-
-</style> -->
